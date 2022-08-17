e240f64c7922a7f3002d17efcea87bb3d15d0daf
UnMarchand <sebastian.jakubiak@sensonar.com>
2022-08-17 15:18:40 +0200

    big upd

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,38 +1,44 @@
-require('dotenv').config();
 const basePath = process.cwd();
-const fs = require("fs");
 const { MODE } = require(`${basePath}/constants/blend_mode.js`);
 const { NETWORK } = require(`${basePath}/constants/network.js`);
 
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Master & Robots 2022";
-const description = "Masters & Robots is designed for the CEOs and Founders, as well as top management teams, and experts. The government officials, decision makers but also creative thinkers will find a substantial dose of knowledge. The event will be beneficial for innovators, and scientists who will have a chance to derive practical insights from the world-class practitioners of the field. That is a great chance for the NGOs representatives, start-up founders and their investors to find some inspiration towards further digital growth. Welcome to the 'What If...' 2022 edition. This time for the very first time we also released web3 NFT Metaverse Tickets. Mint maximum 10 of our NFT's with super-rare GOLD TICKET edition and have fun! ";
+
+//const namePrefix = "Master & Robots 2022";
+//const description = "Masters&Robots is designed for the CEOs and Founders, as well as top management teams, and experts. The government officials, decision makers but also creative thinkers will find a substantial dose of knowledge. The event will be beneficial for innovators, and scientists who will have a chance to derive practical insights from the world-class practitioners of the field. That is a great chance for the NGOs representatives, start-up founders and their investors to find some inspiration towards further digital growth.";
+const namePrefix = "Dream Employer Summit 2022";
+const description = "Nasz świat jest w trakcie zmiany. Półtora roku z pandemią przyniosło nam ogrom zmian i nowych wyzwań. Zgromadziliśmy już nowe doświadczenia, wyznaczamy sobie nowe cele i szukamy całkiem nowych szans. Nie wiemy jeszcze jak będzie wyglądał nasz świat, kiedy już sobie poradzimy z pandemią, nie wiemy jeszcze jak zmienią się modele biznesowe. Przeczuwamy, że zostanie z nami świat pracy hybrydowej, a wraz z nim nowy pracownik i nowy model przywództwa.";
 const baseUri = "ipfs://NewUriToReplace";
 
+const solanaMetadata = {
+  symbol: "YC",
+  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
+  //external_url: "https://mastersandrobots.tech/",
+  external_url: "https://dreamemployer.pl/",
+  /*creators: [
+    {
+      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+      share: 100,
+    },
+  ],*/
+};
+
+// If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 50, //600
+    growEditionSizeTo: 5, //600
     layersOrder: [
       { name: "Event" },
-      { name: "Ticket" },
-      { name: "Speaker" },
-      { name: "What if" },
-      { name: "Partner" },
-      { name: "Class" },
-      //{ name: "Sustainable" },
-      { name: "Digit_0" },
-      { name: "Digit_1" },
-      { name: "Digit_2" },
-      { name: "Digit_3" },
-      { name: "Digit_4" },
-      { name: "Digit_5" },
-      { name: "Digit_6" },
-      { name: "Digit_7" },
-      { name: "Digit_8" },
-      { name: "Digit_9" },
-      //{ name: "Robot" },    
+      { name: "Logo" },
+      { name: "Blip" },
+      { name: "Hand Sign" },
+      { name: "Mindset" },
+      { name: "Sustainable" },
+      { name: "AI" },
+      { name: "Time" },
+      { name: "Religion" },
     ],
   },
 ];
@@ -42,77 +48,9 @@ const shuffleLayerConfigurations = false;
 const debugLogs = false;
 
 const format = {
-  width: 1754, //1754 × 923
-  height: 923,
+  width: 1024,
+  height: 1024,
   smoothing: false,
-};
-
-const extraMetadata = {
-  external_url: "https://mastersandrobots.tech/" // Replace with your website or remove this line if you do not have one.
-};
-
-// NFTPort Info
-
-
-// ** REQUIRED **
-const AUTH = process.env.NFTPORT_API_KEY; // Set this in the .env file to prevent exposing your API key when pushing to Github
-const LIMIT = 2; // Your API key rate limit
-const CHAIN = 'rinkeby'; // only rinkeby, polygon, or ethereum
-
-// REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'Master & Robots 2022';
-const CONTRACT_SYMBOL = 'M&R';
-const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = '0x5BaD5f5Dfd45546eA515ae9484C5c695864Fc54b';
-const TREASURY_ADDRESS = '0x5BaD5f5Dfd45546eA515ae9484C5c695864Fc54b';
-const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
-
-// REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-08-30T07:30:00+00:00"; // to CET +2 / This is required. Eg: 2022-02-08T11:30:48+00:00
-
-// OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xc289A836CFCBcD40a11926167663A387deca08d4"; // Address that will receive the royalty
-const BASE_URI = null; // only update if you want to manually set the base uri
-const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
-
-// ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
-
-// Generic Metadata is optional if you want to reveal your NFTs
-const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
-const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "Which one of the our NFT ticket's you mint?"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafybeifumqreds27chsu6cr5soimqkgo26zozpw2seo5fjv5jhqeuic5wa"; // Replace with your generic image that will display for all NFTs pre-reveal.
-
-// Automatically set contract address if deployed using the deployContract.js script
-try {
-  const rawContractData = fs.readFileSync(
-    `${basePath}/build/contract/_contract.json`
-  );
-  const contractData = JSON.parse(rawContractData);
-  if (contractData.response === "OK") {
-    CONTRACT_ADDRESS = contractData.contract_address;
-  }
-} catch (error) {
-  // Do nothing, falling back to manual contract address
-}
-// END NFTPort Info
-
-const solanaMetadata = {
-  symbol: "YC",
-  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
-  external_url: "https://www.youtube.com/c/hashlipsnft",
-  creators: [
-    {
-      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
-      share: 100,
-    },
-  ],
 };
 
 const gif = {
@@ -140,10 +78,15 @@ const pixelFormat = {
 };
 
 const background = {
-  generate: true,
+  generate: false,
   brightness: "80%",
   static: false,
   default: "#000000",
+};
+
+const extraMetadata = {
+  //external_url: "https://mastersandrobots.tech/",
+  external_url: "https://dreamemployer.pl/",
 };
 
 const rarityDelimiter = "#";
@@ -166,6 +109,57 @@ const preview_gif = {
   imageName: "preview.gif",
 };
 
+// NFTPort Info
+
+// ** REQUIRED **
+const AUTH = process.env.NFTPORT_API_KEY; // Set this in the .env file to prevent exposing your API key when pushing to Github
+const LIMIT = 2; // Your API key rate limit
+const CHAIN = 'rinkeby'; // only rinkeby, polygon, or ethereum
+
+// REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
+const CONTRACT_NAME = 'CRYPTOPUNKS';
+const CONTRACT_SYMBOL = 'CP';
+const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
+const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
+const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
+const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
+const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+
+// REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
+const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+
+// OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
+const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const BASE_URI = null; // only update if you want to manually set the base uri
+const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
+const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+
+// ** OPTIONAL **
+let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+
+// Generic Metadata is optional if you want to reveal your NFTs
+const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
+const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
+const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+
+// Automatically set contract address if deployed using the deployContract.js script
+try {
+  const rawContractData = fs.readFileSync(
+    `${basePath}/build/contract/_contract.json`
+  );
+  const contractData = JSON.parse(rawContractData);
+  if (contractData.response === "OK") {
+    CONTRACT_ADDRESS = contractData.contract_address;
+  }
+} catch (error) {
+  // Do nothing, falling back to manual contract address
+}
+// END NFTPort Info
+
 module.exports = {
   format,
   baseUri,
@@ -185,27 +179,4 @@ module.exports = {
   solanaMetadata,
   gif,
   preview_gif,
-  AUTH,
-  LIMIT,
-  CONTRACT_ADDRESS,
-  OWNER_ADDRESS,
-  TREASURY_ADDRESS,
-  CHAIN,
-  GENERIC,
-  GENERIC_TITLE,
-  GENERIC_DESCRIPTION,
-  GENERIC_IMAGE,
-  CONTRACT_NAME,
-  CONTRACT_SYMBOL,
-  METADATA_UPDATABLE,
-  ROYALTY_SHARE,
-  ROYALTY_ADDRESS,
-  MAX_SUPPLY,
-  MINT_PRICE,
-  TOKENS_PER_MINT,
-  PRESALE_MINT_START_DATE,
-  PUBLIC_MINT_START_DATE,
-  BASE_URI,
-  PREREVEAL_TOKEN_URI,
-  PRESALE_WHITELISTED_ADDRESSES
 };
---
+++ backend/src/config_mr.js
@@ -0,0 +1,212 @@
+require('dotenv').config();
+const basePath = process.cwd();
+const fs = require("fs");
+const { MODE } = require(`${basePath}/constants/blend_mode.js`);
+const { NETWORK } = require(`${basePath}/constants/network.js`);
+
+const network = NETWORK.eth;
+
+// General metadata for Ethereum
+const namePrefix = "Master & Robots 2022";
+const description = "Masters & Robots is designed for the CEOs and Founders, as well as top management teams, and experts. The government officials, decision makers but also creative thinkers will find a substantial dose of knowledge. The event will be beneficial for innovators, and scientists who will have a chance to derive practical insights from the world-class practitioners of the field. That is a great chance for the NGOs representatives, start-up founders and their investors to find some inspiration towards further digital growth. Welcome to the 'What If...' 2022 edition. This time for the very first time we also released web3 NFT Metaverse Tickets. Mint maximum 10 of our NFT's with super-rare GOLD TICKET edition and have fun! ";
+const baseUri = "ipfs://NewUriToReplace";
+
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 10, //600
+    layersOrder: [
+      { name: "Event" },
+      { name: "Digit_0" },
+      { name: "Digit_1" },
+      { name: "Digit_2" },
+      { name: "Digit_3" },
+      { name: "Digit_4" },
+      { name: "Digit_5" },
+      { name: "Digit_6" },
+      { name: "Digit_7" },
+      { name: "Digit_8" },
+      { name: "Digit_9" },
+      { name: "Ticket" },
+      { name: "Master" },
+      //{ name: "Robot" },
+      { name: "What if" },
+      { name: "Partner" },
+      { name: "Class" },
+      //{ name: "Sustainable" },
+      //{ name: "Robot" },    
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 1754, //1754 × 923
+  height: 923,
+  smoothing: false,
+};
+
+const extraMetadata = {
+  external_url: "https://mastersandrobots.tech/" // Replace with your website or remove this line if you do not have one.
+};
+
+// NFTPort Info
+
+
+// ** REQUIRED **
+const AUTH = process.env.NFTPORT_API_KEY; // Set this in the .env file to prevent exposing your API key when pushing to Github
+const LIMIT = 2; // Your API key rate limit
+const CHAIN = 'rinkeby'; // only rinkeby, polygon, or ethereum
+
+// REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
+const CONTRACT_NAME = 'Master & Robots 2022';
+const CONTRACT_SYMBOL = 'M&R';
+const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
+const OWNER_ADDRESS = '0x5BaD5f5Dfd45546eA515ae9484C5c695864Fc54b';
+const TREASURY_ADDRESS = '0x5BaD5f5Dfd45546eA515ae9484C5c695864Fc54b';
+const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
+const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+
+// REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
+const PUBLIC_MINT_START_DATE = "2022-10-04T07:30:00+00:00"; // to CET +2 / This is required. Eg: 2022-02-08T11:30:48+00:00
+
+// OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
+const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_ADDRESS = "0xc289A836CFCBcD40a11926167663A387deca08d4"; // Address that will receive the royalty
+const BASE_URI = null; // only update if you want to manually set the base uri
+const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
+const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+
+// ** OPTIONAL **
+let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+
+// Generic Metadata is optional if you want to reveal your NFTs
+const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
+const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
+const GENERIC_DESCRIPTION = "Which one of the our NFT ticket's you mint?"; // Replace with what you want the generic descriptions to say.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafybeifumqreds27chsu6cr5soimqkgo26zozpw2seo5fjv5jhqeuic5wa"; // Replace with your generic image that will display for all NFTs pre-reveal.
+
+// Automatically set contract address if deployed using the deployContract.js script
+try {
+  const rawContractData = fs.readFileSync(
+    `${basePath}/build/contract/_contract.json`
+  );
+  const contractData = JSON.parse(rawContractData);
+  if (contractData.response === "OK") {
+    CONTRACT_ADDRESS = contractData.contract_address;
+  }
+} catch (error) {
+  // Do nothing, falling back to manual contract address
+}
+// END NFTPort Info
+
+const solanaMetadata = {
+  symbol: "YC",
+  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
+  external_url: "https://www.youtube.com/c/hashlipsnft",
+  creators: [
+    {
+      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+      share: 100,
+    },
+  ],
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+  gif,
+  preview_gif,
+  AUTH,
+  LIMIT,
+  CONTRACT_ADDRESS,
+  OWNER_ADDRESS,
+  TREASURY_ADDRESS,
+  CHAIN,
+  GENERIC,
+  GENERIC_TITLE,
+  GENERIC_DESCRIPTION,
+  GENERIC_IMAGE,
+  CONTRACT_NAME,
+  CONTRACT_SYMBOL,
+  METADATA_UPDATABLE,
+  ROYALTY_SHARE,
+  ROYALTY_ADDRESS,
+  MAX_SUPPLY,
+  MINT_PRICE,
+  TOKENS_PER_MINT,
+  PRESALE_MINT_START_DATE,
+  PUBLIC_MINT_START_DATE,
+  BASE_URI,
+  PREREVEAL_TOKEN_URI,
+  PRESALE_WHITELISTED_ADDRESSES
+};
